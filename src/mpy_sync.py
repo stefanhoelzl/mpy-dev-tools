20df5de2e0b4a06cd97119a61cca222b369e8d79
Stefan Hölzl <stefan.hoelzl@gmx.de>
2017-09-16 17:11:56 +0200

    delete files not in source folder from device at synchronization

--- src/mpy_sync.py
+++ src/mpy_sync.py
@@ -2,6 +2,7 @@ import os
 import shutil
 from pathlib import Path
 
+
 class SyncOperation(object):
     def __init__(self, path):
         self.path = path
@@ -15,7 +16,8 @@ class FileDeleted(SyncOperation): pass
 class DirectoryCreated(SyncOperation): pass
 class DirectoryDeleted(SyncOperation): pass
 
-def sync(src, dest):
+
+def sync(src, dest, cleanup=True):
     last_sync = src / Path('.last_sync')
     if last_sync.is_file():
         last_sync_time = os.stat(str(last_sync)).st_mtime
@@ -24,18 +26,34 @@ def sync(src, dest):
 
     src = Path(src)
     dest = Path(dest)
-    for f in src.glob('**/*'):
-        mtime = os.stat(str(f)).st_mtime
+    for f_src in src.glob('**/*'):
+        mtime = os.stat(str(f_src)).st_mtime
         if mtime > last_sync_time:
-            relative = f.relative_to(src)
+            relative = f_src.relative_to(src)
             f_dest = dest / relative
 
-            if f.is_dir() and not f.exists():
+            if f_src.is_dir() and not f_dest.exists():
                 f_dest.mkdir(parents=True, exist_ok=True)
                 yield DirectoryCreated(relative)
-            elif not f.is_dir():
-                shutil.copy(str(f), str(f_dest))
-                yield FileUpdated(relative)
+            elif f_src.is_file():
+                created = False
+                if not f_dest.exists():
+                    created = True
+                shutil.copy(str(f_src), str(f_dest))
+                if created:
+                    yield FileCreated(relative)
+                else:
+                    yield FileUpdated(relative)
+
+    for f_dest in dest.glob('**/*'):
+        relative = f_dest.relative_to(dest)
+        f_src = src / relative
+        if f_dest.is_file() and not f_src.exists():
+            f_dest.unlink()
+            yield FileDeleted(relative)
+        elif f_dest.is_dir() and not f_src.exists():
+            shutil.rmtree(f_dest)
+            yield DirectoryDeleted(relative)
 
     last_sync.touch(exist_ok=True)
 
